862b066375e8c1a115d6bd226bd79e37c65b7d25
Пивонер <nikguz7@mail.ru>
2025-11-01 21:24:06 +0500

    Добавьте файлы проекта.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Laba18_MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Laba18_MAUI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Laba18_MAUI.Pages")]
