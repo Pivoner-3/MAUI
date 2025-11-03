7d7089ecdbe8f7cc55841289114cb8e2c85f67ee
Пивонер <nikguz7@mail.ru>
2025-11-03 17:44:21 +0500

    The penultimate point
    Added an interface

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,10 +3,10 @@
     public partial class MainPage : ContentPage
     {
         int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+            this.BackgroundColor = Colors.White;
         }
 
         private void OnCounterClicked(object? sender, EventArgs e)
@@ -17,6 +17,17 @@
                 CounterBtn.Text = $"Clicked {count} time";
             else
                 CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private void OnCounterClicked1(object? sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn1.Text = $"Clicked {count} time";
+            else
+                CounterBtn1.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
